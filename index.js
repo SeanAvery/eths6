44da5de3b53e435cf22aa03f66c86023db67d274
Sean Pollock <seanpollock3344@gmail.com>
2017-09-01 20:47:47 -0400

    wip. compile scaffold

--- index.js
+++ index.js
@@ -24,15 +24,29 @@ export default class Eths6 {
     if(!compiled) await this.compileContract()
   }
 
-  async checkcompiled() {
+  async checkCompiled() {
     fs.stat(`${process.cwd()}/${this.file}.compiled.json`, (err, stat) => {
       if (err) return false
-      console.log('stat', stat)
+      console.log('### compiled exists', stat)
       return true
     })
   }
 
   async compileContract() {
     console.log('compiling contract!!')
+    try {
+      const data = await getContractData()
+      await solcCompile()
+    } catch (err) {
+      console.log('### Error compiling contract', err)
+    }
+
+  }
+
+  async getContractData() {
+    fs.readFile(`${process.cwd()}/${this.file}.sol`, (err, data) => {
+      if(err) throw new Error('### Could not get contract data')
+      return data.toString()
+    })
   }
 }
