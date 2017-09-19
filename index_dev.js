f4af6ebfbbbf16d19131052d59183851b4c5ea17
Sean Pollock <seanpollock3344@gmail.com>
2017-09-19 09:45:49 -0700

    check compiled working

--- index_dev.js
+++ index_dev.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 export default class Eths6 {
   constructor(params) {
     if (!params.file) throw new Error('### no file name given')
@@ -8,16 +10,34 @@ export default class Eths6 {
     }
     this.file = params.file
     this.cwd = params.cwd
-    this.state = { }  // memory tree that is saved to disk
+    this.state = {}  // memory tree that is saved to disk
+    if (params.setup) this.setup()
+  }
+
+  async setup() {
+    try {
+      await this.compile()
+    } catch (err) {
+      console.log('### ERROR in setup', err)
+    }
   }
 
   async compile() {
-
+    try {
+      const compiled = await this.checkCompiled()
+      console.log('compiled', compiled)
+    } catch (err) {
+      console.log('### ERROR compiling contract', err)
+    }
   }
 
-  async deploy() {
-
-  }
-
-
+  checkCompiled() {
+    return new Promise((res, rej) => {
+      fs.stat(`${this.cwd}/${this.file}.compiled.json`, (err, stat) => {
+        if (err) res(false)
+        console.log('### compiled already exists')
+        res(true)
+      })
+    })
+   }
 }
